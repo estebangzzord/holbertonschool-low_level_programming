0aea29176b52be20ac6386d8d3332684ab6b14eb
estebangzzord <bambanini@gmail.com>
2023-05-15 18:12:19 -0700

    a

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,24 +1,47 @@
 #include "search_algos.h"
-
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
+ * recursive_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
  *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
- * Return: Always EXIT_SUCCESS
+ * Return: index of the number
  */
-int linear_search(int *array, size_t size, int value)
+int recursive_search(int *array, size_t size, int value)
 {
-int i;
-if (array == NULL)
+size_t half = size / 2;
+size_t i;
+if (array == NULL || size == 0)
 return (-1);
-for (i = 0; i < (int)size; i++)
-{
-printf("Value checked array[%u] = [%d]\n", i, array[i]);
-if (value == array[i])
-return (i);
+printf("Searching in array");
+for (i = 0; i < size; i++)
+printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+printf("\n");
+if (half && size % 2 == 0)
+half--;
+if (value == array[half])
+return ((int)half);
+if (value < array[half])
+return (recursive_search(array, half, value));
+half++;
+return (recursive_search(array + half, size - half, value) + half);
 }
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+int index;
+index = recursive_search(array, size, value);
+if (index >= 0 && array[index] != value)
 return (-1);
+return (index);
 }
